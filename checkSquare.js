b9e6f16fcc520ebd7fa4610ff21d764450682a75
Md Anas Sabah <mdanassabah@gmail.com>
2023-08-17 00:14:08 +0530

    optimized

--- checkSquare.js
+++ checkSquare.js
@@ -3,6 +3,8 @@
 /*
 input-> arr1=[1,2,3,4], arr2=[1,9,4,16]  
 output-> true
+input-> arr1=[1,2,4,2], arr2=[1,4,4,16]  
+output-> true
 */
 
 function square(arr1, arr2) {
@@ -23,4 +25,32 @@ function square(arr1, arr2) {
   }
   return true;
 }
-console.log(square([1, 2, 3, 4], [1, 4, 9, 16]));
+// console.log(square([1, 2, 3, 4], [1, 4, 9, 16])); // Time Complexity => O(n^2)
+// console.log(square([1, 2, 4, 2], [1, 4, 4, 16])); // Time Complexity => O(n^2)
+
+function squareOptimize(array1, array2) {
+  // time complexity-> O(n)
+  let map1 = {};
+  let map2 = {};
+
+  for (item of array1) {
+    map1[item] = (map1[item + 1] || 0) + 1;
+  }
+  for (item1 of array2) {
+    map2[item1] = (map2[item1 + 1] || 0) + 1;
+  }
+  for (let key in map1) {
+    if (!map2[key * key]) {
+      // Obj key compare in terms of square
+      return false;
+    }
+    if (map1[key] !== map2[key * key]) {
+      // value compare
+      return false;
+    }
+  }
+  return true;
+}
+
+console.log(squareOptimize([1, 2, 4, 2], [1, 4, 4, 16])); // true
+console.log(squareOptimize([1, 2, 4, 2], [1, 4, 4, 25])); // false
