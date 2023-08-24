825c1bbf8e6cf977032339ca2767d0cab38bdb24
Md Anas Sabah <mdanassabah@gmail.com>
2023-08-24 12:03:47 +0530

    memoize

--- Memoize.js
+++ Memoize.js
@@ -7,6 +7,15 @@ fib accepts a single integer n and returns 1 if n <= 1 or fib(n - 1) + fib(n - 2
 factorial accepts a single integer n and returns 1 if n <= 1 or factorial(n - 1) * n otherwise.
 */
 
+/*  
+APPROACH:
+1. we use an object cacheObject to store the cached results.
+2.Each time the memoized function is called with a set of arguments, those arguments are converted to a JSON string and used as a key to look up the cached result.
+3. If the key is found in the cacheObject, the cached result is returned.
+4. Otherwise, the original function is called with the arguments, and the result is stored in the cacheObject and returned.
+
+*/
+
 function memoize(fn) {
   const cache = {};
   return function (...args) {
@@ -31,3 +40,11 @@ console.log(memoizedFn(2, 3)); // 5
 console.log(memoizedFn(2, 3)); // 5
 console.log(memoizedFn(4, 3)); // 7
 console.log(callCount); // 2
+
+/* 
+Q: What is Caching?
+Ans: Caching is the process of storing data in a temporary storage area, known as cache, to improve performance and reduce the amount of times  data needs to be fetched from the original source
+In a 5year old language caching is like saving a cookie or a toy in your pocket so you can quickly find it later when you want to use it again. It helps make things faster and easier to find!
+
+
+*/
